44f4658b962f51f0efda550a5beb75b34f8526fd
XAMEUS <xam.master.3d@gmail.com>
2016-05-03 11:16:58 +0200

    msg_par_jour

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import operator
+import datetime
 from Message import Message
 from Mailbox import Mailbox
 
@@ -11,7 +12,9 @@ def pays_plus_frequents(path, n):
             d[pays] = 1
         else:
             d[pays] += 1
-    return [p[0] for p in sorted(d.items(), key=operator.itemgetter(1), reverse=True)][:n]
+    t = [p[0] for p in sorted(d.items(), key=operator.itemgetter(1), reverse=True)][:n]
+    for e in t:
+        print(e)
 
 def cote_moy_spam(path):
     mb = Mailbox(path)
@@ -20,12 +23,24 @@ def cote_moy_spam(path):
     for m in mb:
         s += float(m.mail.get("X-Spam-Score"))
         i += 1
-    return s / i
+    print(s / i)
 
 def msg_par_jour(path):
     mb = Mailbox(path)
-    return 0
+    t = [0, 0, 0, 0, 0, 0, 0]
+    for m in mb:
+        d = datetime.datetime(m.Date[0], m.Date[1], m.Date[2], m.Date[3], m.Date[4], m.Date[5])
+        t[d.weekday()] += 1
+    print("lundi: " + str(t[0]))
+    print("mardi: " + str(t[1]))
+    print("mercredi: " + str(t[2]))
+    print("jeudi: " + str(t[3]))
+    print("vendredi: " + str(t[4]))
+    print("samedi: " + str(t[5]))
+    print("dimanche: " + str(t[6]))
+
 
 if __name__ == "__main__":
-    print(pays_plus_frequents("Messages", 5))
-    print(cote_moy_spam("Messages"))
+    pays_plus_frequents("Messages", 5)
+    cote_moy_spam("Messages")
+    msg_par_jour("Messages")
